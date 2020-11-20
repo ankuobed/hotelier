d864d91021b42f7f263e62f523ab984538ba7a13
Obed Anku <ankuobed@gmail.com>
2020-11-20 20:39:39 +0000

    add sign in page

---
+++ src/components/SingIn/SignIn.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { Avatar, Checkbox } from '@material-ui/core'
+import { EmailOutlined, VpnKeyOutlined } from '@material-ui/icons'
+import './SignIn.css'
+
+const SignIn = () => {
+    useEffect(() => {
+        document.title = 'Sign In'
+    })
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [remember, setRemember] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div className='signInCon container'>
+            <form onSubmit={handleSubmit} className='signIn'>
+
+                <div className='avatarCon'>
+                    <Avatar />
+                </div>
+
+                <div className='inputCon'>
+                    <EmailOutlined />
+                    <input 
+                        type='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Email'
+                        autoFocus
+                    />
+                </div>
+
+                <div className='inputCon'>
+                    <VpnKeyOutlined />
+                    <input 
+                        type='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder='password'
+                    />
+                </div>
+
+                <button>SIGN IN</button>
+
+                <div className='remember'>
+                    <Checkbox
+                        checked={remember}
+                        onChange={() => setRemember(!remember)}
+                        color='inherit'
+                    />
+                    <label>Remember me</label>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
